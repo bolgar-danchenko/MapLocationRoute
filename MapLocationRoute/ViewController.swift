6c37132c506971177084849d32f86287f33ba4ef
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-12-03 18:34:12 +0400

    first commit

---
+++ MapLocationRoute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapLocationRoute
+//
+//  Created by Konstantin Bolgar-Danchenko on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
